ff8fd6b80c5cfdc8a1f5896d52106a3376f63572
martyewings <martyewings@gmail.com>
2022-02-28 10:03:15 +0000

    PDK compatibility/linting/cloud_ci

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 require 'singleton'
+require 'serverspec'
+require 'puppetlabs_spec_helper/module_spec_helper'
+include PuppetLitmus
 
-class Helper
-  include Singleton
-  include PuppetLitmus
+RSpec.configure do |c|
+  c.mock_with :rspec
+  c.before :suite do
+    # Download the plugins to ensure resources
+    PuppetLitmus::PuppetHelpers.run_shell('/opt/puppetlabs/bin/puppet plugin download')
+  end
 end
